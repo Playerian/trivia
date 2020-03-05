e49f2fee476a1d2806abad11165a4d4633b23e84
Student <aa583045912@gmail.com>
2020-03-04 16:41:15 -0800

    Updates

---
+++ precache-manifest.079f5d569d2cef5bd69667f5276943c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed65d760db680813b8c",
+    "url": "/studio2/static/css/main.0c2459bd.chunk.css"
+  },
+  {
+    "revision": "fed65d760db680813b8c",
+    "url": "/studio2/static/js/main.fed65d76.chunk.js"
+  },
+  {
+    "revision": "0cd50ab3f6d9fe7536b4",
+    "url": "/studio2/static/js/1.0cd50ab3.chunk.js"
+  },
+  {
+    "revision": "3a8a3bf4dde7e013c37d",
+    "url": "/studio2/static/js/runtime~main.3a8a3bf4.js"
+  },
+  {
+    "revision": "fc3058fae6a780428ef59910e40f919a",
+    "url": "/studio2/index.html"
+  }
+];
